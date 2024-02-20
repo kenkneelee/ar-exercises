cff47de738779d478c2c6970e0e228b875c4fcca
Kenny Lee <kenny.lee927@gmail.com>
2024-02-20 18:22:14 -0500

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter store name: "
+@store_name = gets.chomp
+
+add_store = Store.create(name: @store_name, mens_apparel: true, womens_apparel: false, annual_revenue: 240000)
+
+unless add_store.save
+  puts "Error: "
+  add_store.errors.full_messages.each do |message|
+      puts message
+  end
+end
